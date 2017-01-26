73c8e2f9a9b61a7380a7216a9b29d92c870e2e0e
tkonk46 <tessa.konkol@gmail.com>
2017-01-26 16:03:19 -0600

    Content added
    All folders added

---
+++ Models/EmployeeGetList_ResultController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NCCTestProject.Models
+{
+    public class EmployeeController : Controller
+    {
+        private northwndEntities1 db = new northwndEntities1();
+
+        // GET: EmployeeGetList_Result
+        public ActionResult Index(bool? active)
+        {
+            var results = db.EmployeeGetList(active).Select(x => new EmployeeModel { EmployeeID = x.EmployeeID, FirstName = x.FirstName, LastName = x.LastName, Gift = "", HireDate = x.HireDate   }).ToList();
+            foreach(var result in results)
+            {
+                DateTime currentDate = DateTime.Now;
+                System.TimeSpan difference = currentDate.Subtract(result.HireDate.Value);
+                int differenceInDays = difference.Days;
+                if (differenceInDays >= 5475)
+                {
+                    result.Gift = "30 day paid vacation";
+                    
+                }
+                else if(differenceInDays>=3650)
+                {
+                    result.Gift = "watch";
+                }
+                else if(differenceInDays>=1825)
+                {
+                    result.Gift = "Tiffany pyramid";
+                }
+                else
+                {
+                    result.Gift = "Employee is not yet eligible.";
+                }
+                
+
+            }
+            //return View(results);
+            return Json(results, JsonRequestBehavior.AllowGet);
+        }
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NCCTestProject.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? HireDate { get; set; }
+        public string Gift { get; set; }
+    }
+}
